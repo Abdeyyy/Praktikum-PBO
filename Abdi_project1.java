28c3e7b38e15f58987167434f6efa9b4f6944e18
Abdi <abdillahaziz182@gmail.com>
2025-08-26 18:03:59 +0700

    Add files via upload

---
+++ Abdi_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abdi_project1;
+
+/**
+ *
+ * @author Advan
+ */
+public class Abdi_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Muhammad Abdillah Aziz");
+        System.out.println("NPM : 2420506017");
+        System.out.println("Alamat: Kramat Utara");
+        System.out.println("No_HP : 08961234567");
+    }
+}
