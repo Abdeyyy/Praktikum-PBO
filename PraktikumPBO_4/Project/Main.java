189c268906fa79260b435e5a736ea5df41377616
Abdi <abdillahaziz182@gmail.com>
2025-09-09 16:57:30 +0700

    Add files via upload

---
+++ PraktikumPBO_4/Project/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.kendaraan;
+/**
+ *
+ * @author Advan
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Mobil mobil1 = new Mobil("Toyota", 300, "V6", 4);
+        mobil1.tampilkanInfoMobil();
+
+    }
+    
+}
+
