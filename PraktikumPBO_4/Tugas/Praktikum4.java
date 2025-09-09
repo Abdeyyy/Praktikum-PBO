2baa53d7307cbd8035ed69b1780ce0ee12f7a391
Abdi <abdillahaziz182@gmail.com>
2025-09-09 16:58:14 +0700

    Add files via upload

---
+++ PraktikumPBO_4/Tugas/Praktikum4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum4;
+
+/**
+ *
+ * @author Advan
+ */
+public class Praktikum4 {
+
+    public static void main(String[] args) {
+        Pekerja pekerja1 = new Pekerja("Citra", 25, "Web Developer", 8000000);
+        System.out.println(pekerja1);
+        
+        pekerja1.setNama("Laura");
+        System.out.println(pekerja1);        
+    }
+}
